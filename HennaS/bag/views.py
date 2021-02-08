61349c38fe3dd613a9d0916c0c85d2d8c2e3f0dc
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-02-08 19:10:28 +0000

    Added product colour functionality

--- HennaS/bag/views.py
+++ HennaS/bag/views.py
@@ -11,12 +11,25 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
+
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if colour:
+        if item_id in list(bag.keys()):
+            if colour in bag[item_id]['items_by_colour'].keys():
+                bag[item_id]['items_by_colour'][colour] += quantity
+            else:
+                bag[item_id]['items_by_colour'][colour] = quantity
+        else:
+            bag[item_id] = {'items_by_colour': {colour: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
