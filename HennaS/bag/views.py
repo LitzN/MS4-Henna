16cc57d14f94aec6fea5a739bf3fa0f207124fad
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-02-09 13:39:01 +0000

    Added modifying bag items and subtotal

--- HennaS/bag/views.py
+++ HennaS/bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
-# Create your views here.
+
 def view_bag(request):
     """ View to return shopping bag """
     return render(request, 'bag/bag.html')
@@ -33,3 +33,51 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ """
+    quantity = int(request.POST.get('quantity'))
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
+
+    bag = request.session.get('bag', {})
+
+    if colour:
+        if quantity > 0:
+            bag[item_id]['items_by_colour'][colour] = quantity
+        else:
+            del bag[item_id]['items_by_colour'][colour]
+            if not bag[item_id]['items_by_colour']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ """
+    try:
+        colour = None
+        if 'product_colour' in request.POST:
+            colour = request.POST['product_colour']
+
+        bag = request.session.get('bag', {})
+
+        if colour:
+            del bag[item_id]['items_by_colour'][colour]
+            if not bag[item_id]['items_by_colour']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
