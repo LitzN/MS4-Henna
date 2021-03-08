226c8c78a76422d6fd1acd92805cd164081f9c42
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-08 17:21:51 +0000

    Added community models

--- HennaS/community/models.py
+++ HennaS/community/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Post(models.Model):
+    user_profile = models.ForeignKey(
+                                     UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='posts')
+    heading = models.CharField(max_length=30, null=False, blank=False)
+    body = models.TextField(null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.heading
+
+
+class Comment(models.Model):
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='comments')
+    for_post = models.ForeignKey('Post', null=False, on_delete=models.CASCADE)
+    body = models.CharField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return self.user_profile.name
