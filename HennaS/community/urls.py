b2adf6cd73333f41d649bd15b675497ed12fe278
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-09 12:51:13 +0000

    Added post form and templates and views for creating and viewing posts

--- HennaS/community/urls.py
+++ HennaS/community/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_posts, name="view_posts")
+    path('', views.view_posts, name="view_posts"),
+    path('add_post/', views.add_post, name="add_post"),
 ]
--- HennaS/community/views.py
+++ HennaS/community/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .models import Post, Comment
+from .forms import PostForm
+
+from django.contrib import messages
 
 
 def view_posts(request):
     """ View to return index page """
     template = 'community/view_posts.html'
+    posts = Post.objects.all()
     context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def add_post(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post Added!')
+            return redirect(reverse('view_posts'))
+        else:
+            messages.error(request, "Post couldn't be added. Please check over your form.")
+    else:
+        form = PostForm()
+    template = 'community/add_post.html'
+    context = {
+        'form': form,
     }
     return render(request, template, context)
