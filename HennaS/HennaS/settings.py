eace442d4f9dc1c77578432ad6cbdcd7be47a5ed
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-22 12:52:05 +0000

    deployment -2

--- HennaS/HennaS/settings.py
+++ HennaS/HennaS/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = '+guo!iyg02awl^=sk@=&d*xjb7-eihuceebh1@90npbt*v$kl9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hennastyles.herokuapp.com', 'localhost'
+]
 
 
 # Application definition
@@ -107,12 +109,17 @@ WSGI_APPLICATION = 'HennaS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
